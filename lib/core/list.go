8639f58f12b078ea3c8b0263bbac6fdff73b41dc
Stig Lindqvist <stojg.lindqvist@gmail.com>
2016-09-18 14:22:57 +1200

    adding code comments on list struct

--- lib/core/list.go
+++ lib/core/list.go
@@ -6,6 +6,7 @@ import (
 	"sync"
 )
 
+// List is the primary resource for adding, removing and changing GameObjects and their components.
 var List *ObjectList
 
 func init() {
@@ -19,6 +20,8 @@ func init() {
 	}
 }
 
+// ObjectList is a struct that contains a list of GameObjects and their components. All creation,
+// removal and changes should be handled by this list so they don't get lost or out of sync.
 type ObjectList struct {
 	sync.Mutex
 	nextID     components.Entity
@@ -30,6 +33,7 @@ type ObjectList struct {
 	deleted    []components.Entity
 }
 
+// Add a GameObject to this list and assign it an unique ID
 func (l *ObjectList) Add(g *GameObject) {
 	l.Lock()
 	defer l.Unlock()
@@ -41,6 +45,7 @@ func (l *ObjectList) Add(g *GameObject) {
 	l.entities[g.id] = g
 }
 
+// Remove a GameObject and all of it's components
 func (l *ObjectList) Remove(g *GameObject) {
 	l.Lock()
 	defer l.Unlock()
@@ -62,130 +67,137 @@ func (l *ObjectList) Remove(g *GameObject) {
 	l.deleted = append(l.deleted, g.id)
 }
 
+// All returns all GameObjects in this list
 func (l *ObjectList) All() []*GameObject {
 	l.Lock()
-	defer l.Unlock()
 	var result []*GameObject
 	for i := range l.entities {
 		result = append(result, l.entities[i])
 	}
+	l.Unlock()
 	return result
 }
 
-// Returns GameObjects tagged tag. Returns nil if no GameObjects was found.
+// FindWithTag returns all GameObjects tagged with tag.
 func (l *ObjectList) FindWithTag(tag string) []*GameObject {
 	l.Lock()
-	defer l.Unlock()
 	var result []*GameObject
 	for i := range l.entities {
 		if l.entities[i].CompareTag(tag) {
 			result = append(result, l.entities[i])
 		}
 	}
+	l.Unlock()
 	return result
 }
 
+// AddGraphic adds a Graphic component to a GameObject
 func (l *ObjectList) AddGraphic(id components.Entity, graphic *Graphic) {
 	l.Lock()
-	defer l.Unlock()
 	graphic.gameObject = l.entities[id]
 	graphic.transform = l.entities[id].transform
 	l.graphics[id] = graphic
+	l.Unlock()
 }
 
+// Graphic returns the Graphic component for a GameObject
 func (l *ObjectList) Graphic(id components.Entity) *Graphic {
-	l.Lock()
-	defer l.Unlock()
 	return l.graphics[id]
 }
 
+// Graphics returns all Graphic components
 func (l *ObjectList) Graphics() []*Graphic {
 	l.Lock()
-	defer l.Unlock()
 	var result []*Graphic
 	for i := range l.graphics {
 		result = append(result, l.graphics[i])
 	}
+	l.Unlock()
 	return result
 }
 
+// AddBody adds a Body component to a GameObject
 func (l *ObjectList) AddBody(id components.Entity, body *Body) {
 	l.Lock()
-	defer l.Unlock()
 	body.gameObject = l.entities[id]
 	body.transform = l.entities[id].transform
 	l.bodies[id] = body
+	l.Unlock()
 }
 
+// Body returns the Body component for a GameObject
 func (l *ObjectList) Body(id components.Entity) *Body {
-	l.Lock()
-	defer l.Unlock()
 	return l.bodies[id]
 }
 
+// Bodies returns all Body components
 func (l *ObjectList) Bodies() []*Body {
 	l.Lock()
-	defer l.Unlock()
 	var result []*Body
 	for i := range l.bodies {
 		result = append(result, l.bodies[i])
 	}
+	l.Unlock()
 	return result
 }
 
+// AddCollision adds a Collision component to a GameObject
 func (l *ObjectList) AddCollision(id components.Entity, collision *Collision) {
 	l.Lock()
-	defer l.Unlock()
 	collision.gameObject = l.entities[id]
 	collision.transform = l.entities[id].transform
 	l.collisions[id] = collision
+	l.Unlock()
 }
 
+// Collision returns the Collision component for a GameObject
 func (l *ObjectList) Collision(id components.Entity) *Collision {
-	l.Lock()
-	defer l.Unlock()
 	return l.collisions[id]
 }
 
+// Collisions returns all registered Collision components
 func (l *ObjectList) Collisions() []*Collision {
 	l.Lock()
-	defer l.Unlock()
 	var result []*Collision
 	for i := range l.collisions {
 		result = append(result, l.collisions[i])
 	}
+	l.Unlock()
 	return result
 }
 
-func (l *ObjectList) AddAI(id components.Entity, agent *Agent) {
+// AddAgent adds an Agent component to a GameObject
+func (l *ObjectList) AddAgent(id components.Entity, agent *Agent) {
 	l.Lock()
-	defer l.Unlock()
 	agent.gameObject = l.entities[id]
 	l.agents[id] = agent
+	l.Unlock()
 }
 
+// Agent returns the Agent component for a GameObject
 func (l *ObjectList) Agent(id components.Entity) *Agent {
-	l.Lock()
-	defer l.Unlock()
 	return l.agents[id]
 }
 
+// Agents returns all registered Agent components
 func (l *ObjectList) Agents() []*Agent {
 	l.Lock()
-	defer l.Unlock()
 	var result []*Agent
 	for i := range l.agents {
 		result = append(result, l.agents[i])
 	}
+	l.Unlock()
 	return result
 }
 
+// Deleted returns a list of GameObject IDs that has been deleted/removed
 func (l *ObjectList) Deleted() []components.Entity {
 	l.Lock()
 	defer l.Unlock()
 	return l.deleted
 }
 
+// ClearDeleted clears the list of deleted GameObjects
 func (l *ObjectList) ClearDeleted() {
 	l.Lock()
 	defer l.Unlock()
