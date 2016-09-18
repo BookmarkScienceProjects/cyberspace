4558db09031e2d52825e6dda1596f35e15fe3130
Stig Lindqvist <stojg.lindqvist@gmail.com>
2016-09-18 14:08:58 +1200

    cleanup and comments on the rigid body struct

--- lib/core/body.go
+++ lib/core/body.go
@@ -5,6 +5,7 @@ import (
 	"sync"
 )
 
+// NewBody returns a new rigidbody that is primarily used for simulating physics
 func NewBody(invMass float64) *Body {
 	body := &Body{
 		velocity:                  &vector.Vector3{},
@@ -26,12 +27,14 @@ func NewBody(invMass float64) *Body {
 		SleepEpsilon:              0.00001,
 	}
 
-	it := &vector.Matrix3{}
-	it.SetBlockInertiaTensor(&vector.Vector3{1, 1, 1}, 1/invMass)
-	body.SetInertiaTensor(it)
+	inertiaTensor := &vector.Matrix3{}
+	// hard coded cube inertia tensor for now
+	inertiaTensor.SetBlockInertiaTensor(&vector.Vector3{1, 1, 1}, 1/invMass)
+	body.setInertiaTensor(inertiaTensor)
 	return body
 }
 
+// Body is a struct that contains data for doing rigid body physics simulation
 type Body struct {
 	Component
 	sync.Mutex
@@ -114,50 +117,58 @@ type Body struct {
 	Forces       *vector.Vector3
 }
 
-func (g *Body) Position() *vector.Vector3 {
-	return g.transform.position
+// Position returns the position of the transform for this body
+func (rb *Body) Position() *vector.Vector3 {
+	return rb.transform.position
 }
 
-func (g *Body) Rotation() *vector.Vector3 {
-	return g.rotation
+// Orientation returns the current orientation of the transform for this body
+func (rb *Body) Orientation() *vector.Quaternion {
+	return rb.transform.orientation
 }
 
-func (g *Body) MaxRotation() float64 {
-	return g.maxRotation
+// Rotation returns the current rotation (the angular version of velocity) for this body
+func (rb *Body) Rotation() *vector.Vector3 {
+	return rb.rotation
 }
 
-func (g *Body) Orientation() *vector.Quaternion {
-	return g.transform.orientation
+// MaxRotation returns the maximum rotation speed for this body in radians
+func (rb *Body) MaxRotation() float64 {
+	return rb.maxRotation
 }
 
-func (g *Body) MaxAcceleration() *vector.Vector3 {
-	return g.maxAcceleration
+// MaxAcceleration returns the maximum acceleration this body that do
+func (rb *Body) MaxAcceleration() *vector.Vector3 {
+	return rb.maxAcceleration
 }
 
-func (g *Body) Velocity() *vector.Vector3 {
-	return g.velocity
+// Velocity returns the current velocity for this body
+func (rb *Body) Velocity() *vector.Vector3 {
+	return rb.velocity
 }
 
+// Mass returns the mass for this body
 func (rb *Body) Mass() float64 {
 	return 1 / rb.InvMass
 }
 
-func (rb *Body) SetInertiaTensor(inertiaTensor *vector.Matrix3) {
-	rb.InverseInertiaTensor.SetInverse(inertiaTensor)
-}
-
+// AddForce adds a force vector that originating from the center of this body
 func (rb *Body) AddForce(force *vector.Vector3) {
 	rb.ForceAccum.Add(force)
 	rb.SetAwake(true)
 }
 
-func (rb *Body) AddForceAtBodyPoint(ent *Transform, force, point *vector.Vector3) {
+// AddForceAtBodyPoint adds a force that is located at a specific point on this body. The point
+// should be relative to the body, not world space
+func (rb *Body) AddForceAtBodyPoint(transform *Transform, force, point *vector.Vector3) {
 	// convert to coordinates relative to center of mass
-	pt := rb.GetPointInWorldSpace(point)
-	rb.AddForceAtPoint(ent, force, pt)
+	pt := rb.PointInWorldSpace(point)
+	rb.AddForceAtPoint(transform, force, pt)
 	rb.SetAwake(true)
 }
 
+// AddForceAtPoint adds a force that is located at a specific point on this body. The point
+// should be in world space.
 func (rb *Body) AddForceAtPoint(body *Transform, force, point *vector.Vector3) {
 	// convert to coordinates relative to center of mass
 	pt := point.NewSub(body.position)
@@ -166,88 +177,93 @@ func (rb *Body) AddForceAtPoint(body *Transform, force, point *vector.Vector3) {
 	rb.SetAwake(true)
 }
 
+// AddTorque as a torque (spin force) that has it's centre at the body
 func (rb *Body) AddTorque(torque *vector.Vector3) {
 	rb.TorqueAccum.Add(torque)
 	rb.SetAwake(true)
 }
 
+// ClearAccumulators will clear all currently added forces and torque. Should be called after the
+// physics integration is done
 func (rb *Body) ClearAccumulators() {
 	rb.Forces.Clear()
 	rb.ForceAccum.Clear()
 	rb.TorqueAccum.Clear()
 }
 
-func (rb *Body) GetPointInWorldSpace(point *vector.Vector3) *vector.Vector3 {
-	return rb.transformMatrix.TransformVector3(point)
-}
-
-func (rb *Body) getTransform() *vector.Matrix4 {
-	return rb.transformMatrix
-}
-
+// CalculateDerivedData recalculates the bodies internal data such as transformation matrices and
+// inertia tensors, should be called once per physics simulation step to keep the data up to date.
 func (rb *Body) CalculateDerivedData(transform *Transform) {
 	transform.Orientation().Normalize()
 	rb.calculateTransformMatrix(rb.transformMatrix, transform.position, transform.Orientation())
 	rb.transformInertiaTensor(rb.InverseInertiaTensorWorld, transform.Orientation(), rb.InverseInertiaTensor, rb.transformMatrix)
 }
 
+// PointInWorldSpace returns the point of the body in world space coordinates
+func (rb *Body) PointInWorldSpace(bodyRelativePoint *vector.Vector3) *vector.Vector3 {
+	return rb.transformMatrix.TransformVector3(bodyRelativePoint)
+}
+
+// Awake reports if this body is current awake or asleep. If it's asleep it means that it's not
+// moving and it can be skipped during physics simulation to decrease cpu usage
 func (rb *Body) Awake() bool {
 	rb.Lock()
 	defer rb.Unlock()
 	return rb.isAwake
 }
 
+// SetAwake sets this body in an awake or asleep state.
 func (rb *Body) SetAwake(t bool) {
 	rb.Lock()
 	defer rb.Unlock()
 	rb.isAwake = t
 }
 
-/**
- * Internal function to do an intertia tensor transform by a vector.Quaternion.
- * Note that the implementation of this function was created by an
- * automated code-generator and optimizer.
- */
-func (rb *Body) transformInertiaTensor(iitWorld *vector.Matrix3, q *vector.Quaternion, iitBody *vector.Matrix3, rotmat *vector.Matrix4) {
-	t4 := rotmat[0]*iitBody[0] + rotmat[1]*iitBody[3] + rotmat[2]*iitBody[6]
-	t9 := rotmat[0]*iitBody[1] + rotmat[1]*iitBody[4] + rotmat[2]*iitBody[7]
-	t14 := rotmat[0]*iitBody[2] + rotmat[1]*iitBody[5] + rotmat[2]*iitBody[8]
-	t28 := rotmat[4]*iitBody[0] + rotmat[5]*iitBody[3] + rotmat[6]*iitBody[6]
-	t33 := rotmat[4]*iitBody[1] + rotmat[5]*iitBody[4] + rotmat[6]*iitBody[7]
-	t38 := rotmat[4]*iitBody[2] + rotmat[5]*iitBody[5] + rotmat[6]*iitBody[8]
-	t52 := rotmat[8]*iitBody[0] + rotmat[9]*iitBody[3] + rotmat[10]*iitBody[6]
-	t57 := rotmat[8]*iitBody[1] + rotmat[9]*iitBody[4] + rotmat[10]*iitBody[7]
-	t62 := rotmat[8]*iitBody[2] + rotmat[9]*iitBody[5] + rotmat[10]*iitBody[8]
-
-	iitWorld[0] = t4*rotmat[0] + t9*rotmat[1] + t14*rotmat[2]
-	iitWorld[1] = t4*rotmat[4] + t9*rotmat[5] + t14*rotmat[6]
-	iitWorld[2] = t4*rotmat[8] + t9*rotmat[9] + t14*rotmat[10]
-	iitWorld[3] = t28*rotmat[0] + t33*rotmat[1] + t38*rotmat[2]
-	iitWorld[4] = t28*rotmat[4] + t33*rotmat[5] + t38*rotmat[6]
-	iitWorld[5] = t28*rotmat[8] + t33*rotmat[9] + t38*rotmat[10]
-	iitWorld[6] = t52*rotmat[0] + t57*rotmat[1] + t62*rotmat[2]
-	iitWorld[7] = t52*rotmat[4] + t57*rotmat[5] + t62*rotmat[6]
-	iitWorld[8] = t52*rotmat[8] + t57*rotmat[9] + t62*rotmat[10]
+// setInertiaTensor is a utility function to set the inertia tensor correct
+func (rb *Body) setInertiaTensor(inertiaTensor *vector.Matrix3) {
+	rb.InverseInertiaTensor.SetInverse(inertiaTensor)
 }
 
-/**
- * Inline function that creates a transform matrix from a
- * position and orientation.
- */
-func (rb *Body) calculateTransformMatrix(transformMatrix *vector.Matrix4, position *vector.Vector3, orientation *vector.Quaternion) {
+// transformInertiaTensor does an inertia tensor transform by a vector.Quaternion. The result of
+// this transform will be set on the iitWorld matrix
+func (rb *Body) transformInertiaTensor(iitWorld *vector.Matrix3, q *vector.Quaternion, iitBody *vector.Matrix3, rotMat *vector.Matrix4) {
+	t4 := rotMat[0]*iitBody[0] + rotMat[1]*iitBody[3] + rotMat[2]*iitBody[6]
+	t9 := rotMat[0]*iitBody[1] + rotMat[1]*iitBody[4] + rotMat[2]*iitBody[7]
+	t14 := rotMat[0]*iitBody[2] + rotMat[1]*iitBody[5] + rotMat[2]*iitBody[8]
+	t28 := rotMat[4]*iitBody[0] + rotMat[5]*iitBody[3] + rotMat[6]*iitBody[6]
+	t33 := rotMat[4]*iitBody[1] + rotMat[5]*iitBody[4] + rotMat[6]*iitBody[7]
+	t38 := rotMat[4]*iitBody[2] + rotMat[5]*iitBody[5] + rotMat[6]*iitBody[8]
+	t52 := rotMat[8]*iitBody[0] + rotMat[9]*iitBody[3] + rotMat[10]*iitBody[6]
+	t57 := rotMat[8]*iitBody[1] + rotMat[9]*iitBody[4] + rotMat[10]*iitBody[7]
+	t62 := rotMat[8]*iitBody[2] + rotMat[9]*iitBody[5] + rotMat[10]*iitBody[8]
 
-	transformMatrix[0] = 1 - 2*orientation.J*orientation.J - 2*orientation.K*orientation.K
-	transformMatrix[1] = 2*orientation.I*orientation.J - 2*orientation.R*orientation.K
-	transformMatrix[2] = 2*orientation.I*orientation.K + 2*orientation.R*orientation.J
-	transformMatrix[3] = position[0]
+	iitWorld[0] = t4*rotMat[0] + t9*rotMat[1] + t14*rotMat[2]
+	iitWorld[1] = t4*rotMat[4] + t9*rotMat[5] + t14*rotMat[6]
+	iitWorld[2] = t4*rotMat[8] + t9*rotMat[9] + t14*rotMat[10]
+	iitWorld[3] = t28*rotMat[0] + t33*rotMat[1] + t38*rotMat[2]
+	iitWorld[4] = t28*rotMat[4] + t33*rotMat[5] + t38*rotMat[6]
+	iitWorld[5] = t28*rotMat[8] + t33*rotMat[9] + t38*rotMat[10]
+	iitWorld[6] = t52*rotMat[0] + t57*rotMat[1] + t62*rotMat[2]
+	iitWorld[7] = t52*rotMat[4] + t57*rotMat[5] + t62*rotMat[6]
+	iitWorld[8] = t52*rotMat[8] + t57*rotMat[9] + t62*rotMat[10]
+}
 
-	transformMatrix[4] = 2*orientation.I*orientation.J + 2*orientation.R*orientation.K
-	transformMatrix[5] = 1 - 2*orientation.I*orientation.I - 2*orientation.K*orientation.K
-	transformMatrix[6] = 2*orientation.J*orientation.K - 2*orientation.R*orientation.I
-	transformMatrix[7] = position[1]
+// calculateTransformMatrix creates a transform matrix from a position and orientation. The result
+// is be set on the rotMat
+func (rb *Body) calculateTransformMatrix(rotMat *vector.Matrix4, pos *vector.Vector3, orientation *vector.Quaternion) {
 
-	transformMatrix[8] = 2*orientation.I*orientation.K - 2*orientation.R*orientation.J
-	transformMatrix[9] = 2*orientation.J*orientation.K + 2*orientation.R*orientation.I
-	transformMatrix[10] = 1 - 2*orientation.I*orientation.I - 2*orientation.J*orientation.J
-	transformMatrix[11] = position[2]
+	rotMat[0] = 1 - 2*orientation.J*orientation.J - 2*orientation.K*orientation.K
+	rotMat[1] = 2*orientation.I*orientation.J - 2*orientation.R*orientation.K
+	rotMat[2] = 2*orientation.I*orientation.K + 2*orientation.R*orientation.J
+	rotMat[3] = pos[0]
+
+	rotMat[4] = 2*orientation.I*orientation.J + 2*orientation.R*orientation.K
+	rotMat[5] = 1 - 2*orientation.I*orientation.I - 2*orientation.K*orientation.K
+	rotMat[6] = 2*orientation.J*orientation.K - 2*orientation.R*orientation.I
+	rotMat[7] = pos[1]
+
+	rotMat[8] = 2*orientation.I*orientation.K - 2*orientation.R*orientation.J
+	rotMat[9] = 2*orientation.J*orientation.K + 2*orientation.R*orientation.I
+	rotMat[10] = 1 - 2*orientation.I*orientation.I - 2*orientation.J*orientation.J
+	rotMat[11] = pos[2]
 }
