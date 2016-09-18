28ace442482bf3e336b8622a3bbe7d05ccafdecb
Stig Lindqvist <stojg.lindqvist@gmail.com>
2016-09-18 14:30:19 +1200

    add code comment to the graphic component

--- lib/core/graphic.go
+++ lib/core/graphic.go
@@ -1,25 +1,30 @@
 package core
 
+// NewGraphic returns a new Graphic component
 func NewGraphic(model int) *Graphic {
 	return &Graphic{
 		model: model,
 	}
 }
 
+// Graphic component describes various properties on how it's related GameObject should be rendered
 type Graphic struct {
 	Component
 	isRendered bool
 	model      int
 }
 
+// IsRendered return true if the related GameObject has been rendered to the screen before
 func (g *Graphic) IsRendered() bool {
 	return g.isRendered
 }
 
+// SetRendered marks the related GameObject as rendered to the screen
 func (g *Graphic) SetRendered() {
 	g.isRendered = true
 }
 
+// Model returns the model number for the related GameObject
 func (g *Graphic) Model() int {
 	return g.model
 }
