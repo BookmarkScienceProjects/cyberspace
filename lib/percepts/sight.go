bccf727fc86f654698686373cf32234ac9611b38
Stig Lindqvist <stojg.lindqvist@gmail.com>
2017-04-16 15:47:45 +1200

    Add first percepts

---
+++ lib/percepts/sight.go
@@ -0,0 +1,40 @@
+package percepts
+
+import (
+	"github.com/stojg/cyberspace/lib/core"
+	"github.com/stojg/vector"
+	"math"
+)
+
+// Distance checks distance between me and other with a maxDistance and returns the confidence between 0 and 1
+func Distance(me, other *core.GameObject, maxDistance float64) float64 {
+	direction := me.Transform().Position().NewSub(other.Transform().Position())
+	if direction.SquareLength() > maxDistance*maxDistance {
+		return 0
+	}
+	return (maxDistance - direction.Length()) / maxDistance
+}
+
+// InSight checks if other is in me view cone and returns a confidence between 0 and 1.
+// viewCone should be in radians
+func InSight(me, other *core.GameObject, viewCone float64) float64 {
+	direction := other.Transform().Position().NewSub(me.Transform().Position())
+	// two entities in the same position can see each other
+	if direction.SquareLength() == 0 {
+		return 1
+	}
+	// normalise after the above check for efficiency
+	direction.Normalize()
+
+	guardFacing := vector.X().Rotate(me.Transform().Orientation())
+
+	dot := guardFacing.Dot(direction)
+	angle := math.Acos(dot)
+
+	halfViewCone := viewCone / 2
+	confidence := (halfViewCone - angle) / halfViewCone
+	if confidence < 0 {
+		return 0
+	}
+	return confidence
+}
---
+++ lib/percepts/sight_test.go
@@ -0,0 +1,76 @@
+package percepts
+
+import (
+	"github.com/stojg/cyberspace/lib/core"
+	"math"
+	"testing"
+)
+
+func TestDistance(t *testing.T) {
+	me := core.NewGameObject("me")
+	me.Transform().Position().Set(0, 0, 0)
+
+	friend := core.NewGameObject("friend")
+	friend.Transform().Position().Set(10, 0, 0)
+
+	confidence := Distance(me, friend, 20)
+	if confidence != 0.5 {
+		t.Errorf("I should be able see my friend, confidence %0.2f", confidence)
+	}
+}
+
+func TestDistanceTooFar(t *testing.T) {
+	me := core.NewGameObject("me")
+	me.Transform().Position().Set(0, 0, 0)
+
+	friend := core.NewGameObject("friend")
+	friend.Transform().Position().Set(10, 0, 0)
+
+	confidence := Distance(me, friend, 5)
+	if confidence != 0.0 {
+		t.Error("I should not be able see my friend, but see her with confidence %0.2f", confidence)
+	}
+}
+
+func TestViewConeSamePosition(t *testing.T) {
+	me := core.NewGameObject("me")
+	me.Transform().Position().Set(0, 0, 0)
+
+	friend := core.NewGameObject("friend")
+	friend.Transform().Position().Set(0, 0, 0)
+
+	confidence := InSight(me, friend, 3.14)
+	if confidence != 1 {
+		t.Error("Expected that two entities in the same position can see each other")
+	}
+}
+
+func TestViewConeInFront(t *testing.T) {
+	tests := []struct {
+		in  [3]float64
+		out float64
+	}{
+		{[3]float64{0, 0, 0}, 1.0},       // on top
+		{[3]float64{1, 0, 0}, 1.0},       // in front
+		{[3]float64{-1, 0, 0}, 0.0},      // behind
+		{[3]float64{1, 0, 1}, 0.5},       // front-left
+		{[3]float64{1, 0, -1}, 0.5},      // front-right
+		{[3]float64{0.4142, 0, 1}, 0.25}, // front-right
+	}
+
+	me := core.NewGameObject("me")
+	me.Transform().Position().Set(0, 0, 0)
+	for _, test := range tests {
+		friend := core.NewGameObject("friend")
+		friend.Transform().Position().Set(test.in[0], test.in[1], test.in[2])
+		confidence := InSight(me, friend, math.Pi)
+		if equals(test.out, confidence) {
+			t.Errorf("(%1.f,%1.f,%1.f): Expected confidence %0.4f, got %0.4f", test.in[0], test.in[1], test.in[2], test.out, confidence)
+		}
+	}
+}
+
+func equals(a, b float64) bool {
+	const epsilon = 0.0001
+	return a > (b+epsilon) || a < (b-epsilon)
+}
